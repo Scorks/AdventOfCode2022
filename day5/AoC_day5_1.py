a2911ae54390390537dad8881ef8976c02652b50
Caro Strickland <carostrickland321@gmail.com>
2022-12-07 00:24:37 -0500

    Add files via upload

---
+++ day5/AoC_day5_1.py
@@ -0,0 +1,32 @@
+import re
+
+foodList = open('../input/AoC_input_5.txt', 'r')
+lines = foodList.readlines()
+
+stacks = [[],[],[],[],[],[],[],[],[]]
+
+for line in lines:
+    index_x = 0
+    index_y = 1
+    index_z = 2
+    stack = 1
+    if ('[' not in line):
+        if ('from' in line): # the instructions
+             instruction = re.findall(r'\d+', line)
+             takeBoxesFromStack = stacks[int(instruction[1])-1]
+             takeBoxesFromStack = takeBoxesFromStack[:int(instruction[0])]
+             del stacks[int(instruction[1])-1][:int(instruction[0])]
+             for item in takeBoxesFromStack:
+                stacks[int(instruction[2])-1].insert(0, item)
+    else:
+        while (index_z < len(line)):
+            box = line[index_y]
+            index_x += 4
+            index_y += 4
+            index_z += 4
+            if (box != ' '):
+                stacks[stack-1].append(box)
+            stack += 1
+
+for i in stacks:
+  print(i[0])
